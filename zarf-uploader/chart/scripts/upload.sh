54243daa4537dc20ded5358a91e97c376efe50ee
Wayne Starr <Racer159@users.noreply.github.com>
2024-02-22 16:01:00 -0700

    feat: add uploader component (#11)
    * feat: add uploader component
    
    * add packages to gitignore

---
+++ zarf-uploader/chart/scripts/upload.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#####
+# Upload all the PyPI packages
+#####
+python3 -m twine upload --repository-url=http://zarf-gitea-http.zarf.svc.cluster.local:3000/api/packages/###ZARF_GIT_PUSH###/pypi \
+                       --skip-existing \
+                       --verbose \
+                       -u "###ZARF_GIT_PUSH###" \
+                       -p "###ZARF_GIT_AUTH_PUSH###" \
+                       /packages/*  
